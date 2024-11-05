f725e07158c43b6285b9c571c140b7fb44c4eee1
sahil <shp2382@g.rit.edu>
2024-11-04 23:15:41 -0500

    init

---
+++ shared/types/meteor-mongo.d.ts
@@ -0,0 +1,17 @@
+declare module 'meteor/mongo' {
+    import SimpleSchema from 'simpl-schema';
+  
+    module Mongo {
+      export interface Collection<T> {
+        schema: SimpleSchema;
+  
+        attachSchema(schema: SimpleSchema): void;
+  
+        attachJSONSchema(schema: any): void;
+  
+        helpers(methods: object): void;
+  
+        _name: string;
+      }
+    }
+  }
